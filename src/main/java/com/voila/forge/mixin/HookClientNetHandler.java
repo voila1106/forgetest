2e97759e19e28842fe0c7a4eec033db6b1b88ced
voila1106 <voila1106@gmail.com>
2022-04-19 14:37:18 +0800

    no raining effect

--- src/main/java/com/voila/forge/mixin/HookClientNetHandler.java
+++ src/main/java/com/voila/forge/mixin/HookClientNetHandler.java
@@ -19,14 +19,12 @@ import net.minecraft.tileentity.*;
 import net.minecraft.util.*;
 import net.minecraft.util.text.*;
 import net.minecraft.world.*;
-import net.minecraftforge.client.*;
-import org.apache.logging.log4j.*;
+import net.minecraft.world.biome.*;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.*;
 
 import java.util.*;
-import java.util.function.*;
 
 @Mixin(ClientPlayNetHandler.class)
 public abstract class HookClientNetHandler
@@ -47,43 +45,43 @@ abstract class HookGameRenderer  //disable damage screen shake
 @Mixin(LivingRenderer.class)
 abstract class HookLivingRenderer  //damage amount particle
 {
-	private Map<Integer,Float> hs=new HashMap<>();
+	private Map<Integer, Float> hs = new HashMap<>();
 	private World world;
 
-	@Inject(method = "render(Lnet/minecraft/entity/LivingEntity;FFLcom/mojang/blaze3d/matrix/MatrixStack;Lnet/minecraft/client/renderer/IRenderTypeBuffer;I)V",at = @At("HEAD"))
+	@Inject(method = "render(Lnet/minecraft/entity/LivingEntity;FFLcom/mojang/blaze3d/matrix/MatrixStack;Lnet/minecraft/client/renderer/IRenderTypeBuffer;I)V", at = @At("HEAD"))
 	private void r(LivingEntity entity, float yaw, float ticks, MatrixStack stack, IRenderTypeBuffer buffer, int light, CallbackInfo info)
 	{
 		if(!entity.world.equals(world))
 		{
-			hs=new HashMap<>();
-			world=entity.world;
+			hs = new HashMap<>();
+			world = entity.world;
 		}
-		float health=entity.getHealth();
+		float health = entity.getHealth();
 		if(!hs.containsKey(entity.hashCode()))
 		{
-			hs.put(entity.hashCode(),health);
+			hs.put(entity.hashCode(), health);
 			return;
 		}
-		if(health<hs.get(entity.hashCode()))
+		if(health < hs.get(entity.hashCode()))
 		{
-			float damage=(hs.get(entity.hashCode())-health);
-			for(int i = 0; i < Math.min(100,damage); i++)
+			float damage = (hs.get(entity.hashCode()) - health);
+			for(int i = 0; i < Math.min(100, damage); i++)
 			{
 				Minecraft.getInstance().worldRenderer.addParticle(new DamageParticle.DamageParticleData((int)damage),
 					true,
 					entity.getPosX(),
 					entity.getPosYHeight(0.5),
-					entity.getPosZ(),0.1,0,0.1);
+					entity.getPosZ(), 0.1, 0, 0.1);
 			}
 		}
-		hs.put(entity.hashCode(),health);
+		hs.put(entity.hashCode(), health);
 	}
 }
 
 @Mixin(ParticleManager.class)
 abstract class HookParticleManager  // remove some particles
 {
-	@Inject(method = "addParticle",at = @At("HEAD"),cancellable = true)
+	@Inject(method = "addParticle", at = @At("HEAD"), cancellable = true)
 	private void i(IParticleData particleData, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed, CallbackInfoReturnable<Particle> info)
 	{
 		if(particleData.equals(ParticleTypes.DAMAGE_INDICATOR) || particleData.equals(ParticleTypes.ELDER_GUARDIAN))
@@ -95,25 +93,25 @@ abstract class HookParticleManager  // remove some particles
 @Mixin(IngameGui.class)
 abstract class HookIngameGui  // no spam
 {
-	@Inject(method = "sendChatMessage",at = @At("HEAD"),cancellable = true)
-	private void i(ChatType type, ITextComponent cmp, UUID uuid,CallbackInfo info)
+	@Inject(method = "sendChatMessage", at = @At("HEAD"), cancellable = true)
+	private void i(ChatType type, ITextComponent cmp, UUID uuid, CallbackInfo info)
 	{
-		String str= cmp.getString();
+		String str = cmp.getString();
 		if(str.equals(Forgetest.last) && !(Minecraft.getInstance().currentScreen instanceof ChatScreen))
 		{
 			info.cancel();
 		}
-		Forgetest.last=str;
+		Forgetest.last = str;
 	}
 }
 
 @Mixin(Minecraft.class)
 abstract class HookMinecraft  // no spam
 {
-	@Inject(method = "displayGuiScreen",at = @At("HEAD"))
+	@Inject(method = "displayGuiScreen", at = @At("HEAD"))
 	private void i(Screen guiScreenIn, CallbackInfo ci)
 	{
-		Forgetest.last="";
+		Forgetest.last = "";
 	}
 }
 
@@ -141,10 +139,10 @@ abstract class HookItemStack
 @Mixin(Scoreboard.class)
 abstract class HookScoreBoard  // fix score board NPE
 {
-	@Inject(method = "removeTeam",at = @At("HEAD"),cancellable = true)
+	@Inject(method = "removeTeam", at = @At("HEAD"), cancellable = true)
 	private void i(ScorePlayerTeam playerTeam, CallbackInfo ci)
 	{
-		if(playerTeam==null)
+		if(playerTeam == null)
 			ci.cancel();
 	}
 }
@@ -152,9 +150,10 @@ abstract class HookScoreBoard  // fix score board NPE
 @Mixin(SkullTileEntity.class)
 abstract class HookSkullTileEntity  // fix skull texture loading stuck
 {
-	@Shadow private static PlayerProfileCache profileCache;
+	@Shadow
+	private static PlayerProfileCache profileCache;
 
-	@Inject(method = "updateGameProfile",at = @At(value = "INVOKE",
+	@Inject(method = "updateGameProfile", at = @At(value = "INVOKE",
 		target = "Lcom/mojang/authlib/minecraft/MinecraftSessionService;fillProfileProperties(Lcom/mojang/authlib/GameProfile;Z)Lcom/mojang/authlib/GameProfile;"),
 		cancellable = true)
 	private static void i(GameProfile input, CallbackInfoReturnable<GameProfile> info)
@@ -167,7 +166,7 @@ abstract class HookSkullTileEntity  // fix skull texture loading stuck
 @Mixin(SharedConstants.class)
 abstract class HookSharedConstants  // allow all characters
 {
-	@Inject(method = "isAllowedCharacter",at = @At("HEAD"),cancellable = true)
+	@Inject(method = "isAllowedCharacter", at = @At("HEAD"), cancellable = true)
 	private static void i(char character, CallbackInfoReturnable<Boolean> info)
 	{
 		info.setReturnValue(true);
@@ -177,12 +176,10 @@ abstract class HookSharedConstants  // allow all characters
 @Mixin(ClientWorld.class)
 abstract class HookClientWorld
 {
-	@Shadow public abstract DimensionRenderInfo getDimensionRenderInfo();
-
 	/**
 	 * disable portal sound
 	 */
-	@Inject(method = "playSound(DDDLnet/minecraft/util/SoundEvent;Lnet/minecraft/util/SoundCategory;FFZ)V",at = @At("HEAD"),cancellable = true)
+	@Inject(method = "playSound(DDDLnet/minecraft/util/SoundEvent;Lnet/minecraft/util/SoundCategory;FFZ)V", at = @At("HEAD"), cancellable = true)
 	private void i(double x, double y, double z, SoundEvent soundIn, SoundCategory category, float volume, float pitch, boolean distanceDelay, CallbackInfo info)
 	{
 		if(soundIn.equals(SoundEvents.BLOCK_PORTAL_AMBIENT))
@@ -190,8 +187,14 @@ abstract class HookClientWorld
 	}
 }
 
-// TODO: WorldRenderer.renderRainSnow
+@Mixin(Biome.class)
+abstract class HookBiome
+{
+	@Inject(method = "getPrecipitation", at = @At("HEAD"), cancellable = true)
+	private void i(CallbackInfoReturnable<Biome.RainType> info)
+	{
+		info.setReturnValue(Biome.RainType.NONE);
+	}
+}
 
-
-
-// TODO: disable fire effect
+// TODO: check others backpack
