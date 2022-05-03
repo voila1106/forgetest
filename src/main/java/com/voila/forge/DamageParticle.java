c979770747e9e8d4d138cf230a1ba91bfd0e7438
voila1106 <voila1106@gmail.com>
2022-05-03 12:43:54 +0800

    re-format

--- src/main/java/com/voila/forge/DamageParticle.java
+++ src/main/java/com/voila/forge/DamageParticle.java
@@ -12,120 +12,103 @@ import org.apache.logging.log4j.*;
 
 import javax.annotation.*;
 
-public class DamageParticle extends SpriteTexturedParticle
-{
+public class DamageParticle extends SpriteTexturedParticle {
 	int amount;
-	int ma=15;
-	static Logger Log=LogManager.getLogger();
+	int ma = 15;
+	static Logger Log = LogManager.getLogger();
 	IAnimatedSprite sprite;
-	protected DamageParticle(ClientWorld world, double x, double y, double z, double motionX, double motionY, double motionZ,int amount,IAnimatedSprite as)
-	{
-		super(world,x,y,z,motionX,motionY,motionZ);
-		maxAge=2048;
-		canCollide=false;
-		this.amount= Math.max(amount, 0);
-		sprite=as;
-		setSprite(sprite.get(amount,2048));
-		if(this.motionY<0)
-			this.motionY=-this.motionY;
+
+	protected DamageParticle(ClientWorld world, double x, double y, double z, double motionX, double motionY, double motionZ, int amount, IAnimatedSprite as){
+		super(world, x, y, z, motionX, motionY, motionZ);
+		maxAge = 2048;
+		canCollide = false;
+		this.amount = Math.max(amount, 0);
+		sprite = as;
+		setSprite(sprite.get(amount, 2048));
+		if(this.motionY < 0)
+			this.motionY = -this.motionY;
 
 	}
 
 	@Override
-	public void tick()
-	{
+	public void tick(){
 		super.tick();
 		this.motionX *= 0.9;
 		this.motionY *= 0.7;
 		this.motionZ *= 0.9;
 		this.motionY -= 0.02;
-		setSprite(sprite.get(amount,2048));
-		if(age>ma)
+		setSprite(sprite.get(amount, 2048));
+		if(age > ma)
 			setExpired();
 	}
 
 	@Override
-	public IParticleRenderType getRenderType()
-	{
+	public IParticleRenderType getRenderType(){
 		return IParticleRenderType.PARTICLE_SHEET_LIT;
 	}
 
-	public static class DamageParticleData extends ParticleType<DamageParticleData> implements IParticleData
-	{
+	public static class DamageParticleData extends ParticleType<DamageParticleData> implements IParticleData {
 		int amount;
-		public static final IDeserializer<DamageParticleData> deserializer=new IDeserializer<DamageParticleData>()
-		{
+		public static final IDeserializer<DamageParticleData> deserializer = new IDeserializer<DamageParticleData>() {
 			@Override
-			public DamageParticleData deserialize(ParticleType<DamageParticleData> particleTypeIn, StringReader reader) throws CommandSyntaxException
-			{
+			public DamageParticleData deserialize(ParticleType<DamageParticleData> particleTypeIn, StringReader reader) throws CommandSyntaxException{
 				reader.expect(' ');
-				int amount=reader.readInt();
+				int amount = reader.readInt();
 				return new DamageParticleData(amount);
 			}
 
 			@Override
-			public DamageParticleData read(ParticleType<DamageParticleData> particleTypeIn, PacketBuffer buffer)
-			{
+			public DamageParticleData read(ParticleType<DamageParticleData> particleTypeIn, PacketBuffer buffer){
 				return new DamageParticleData(buffer.readInt());
 			}
 		};
 
-		public DamageParticleData(int amount)
-		{
+		public DamageParticleData(int amount){
 			this();
-			this.amount=amount;
+			this.amount = amount;
 		}
 
-		public DamageParticleData()
-		{
+		public DamageParticleData(){
 			super(true, deserializer);
 		}
 
 		@Override
-		public ParticleType<?> getType()
-		{
+		public ParticleType<?> getType(){
 			return TestItem.damage.get();
 		}
 
 		@Override
-		public void write(PacketBuffer buffer)
-		{
+		public void write(PacketBuffer buffer){
 			buffer.writeInt(this.amount);
 		}
 
 		@Override
-		public String getParameters()
-		{
-			return String.format("%s %d", this.getType().getRegistryName(),this.amount);
+		public String getParameters(){
+			return String.format("%s %d", this.getType().getRegistryName(), this.amount);
 		}
 
 		@Override
-		public Codec<DamageParticleData> func_230522_e_()
-		{
+		public Codec<DamageParticleData> func_230522_e_(){
 			return Codec.unit(() -> DamageParticleData.this);
 		}
 
-		public int getAmount()
-		{
+		public int getAmount(){
 			return amount;
 		}
 	}
 
 	@OnlyIn(Dist.CLIENT)
-	public static class Factory implements IParticleFactory<DamageParticleData>
-	{
+	public static class Factory implements IParticleFactory<DamageParticleData> {
 		private final IAnimatedSprite sprites;
 
-		public Factory(IAnimatedSprite sprite)
-		{
-			this.sprites=sprite;
+		public Factory(IAnimatedSprite sprite){
+			this.sprites = sprite;
 		}
 
 		@Nullable
 		@Override
-		public Particle makeParticle(DamageParticleData typeIn, ClientWorld worldIn, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed)
-		{
-			DamageParticle particle=new DamageParticle(worldIn,x,y,z,xSpeed,ySpeed,zSpeed, typeIn.getAmount(),sprites);
+		public Particle makeParticle(DamageParticleData typeIn, ClientWorld worldIn, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed){
+			DamageParticle particle = new DamageParticle(worldIn, x, y, z, xSpeed, ySpeed, zSpeed, typeIn.getAmount(), sprites);
 			return particle;
 		}
 	}
