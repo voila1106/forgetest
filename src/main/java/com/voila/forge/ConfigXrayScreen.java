a7d7ed9d7e4e00ea14212fa93e899357457fe7d1
voila1106 <voila1106@gmail.com>
2022-05-30 11:48:38 +0800

    block suggestion

--- src/main/java/com/voila/forge/ConfigXrayScreen.java
+++ src/main/java/com/voila/forge/ConfigXrayScreen.java
@@ -1,13 +1,19 @@
 package com.voila.forge;
 
+import com.mojang.blaze3d.systems.*;
 import com.mojang.blaze3d.vertex.*;
 import net.minecraft.client.*;
 import net.minecraft.client.gui.*;
 import net.minecraft.client.gui.components.*;
 import net.minecraft.client.gui.screens.*;
+import net.minecraft.client.player.*;
+import net.minecraft.core.*;
 import net.minecraft.network.chat.*;
 import net.minecraft.resources.*;
+import net.minecraft.util.*;
+import net.minecraft.world.item.*;
 import net.minecraft.world.level.block.*;
+import net.minecraft.world.phys.*;
 import net.minecraftforge.registries.*;
 import org.jetbrains.annotations.*;
 import org.lwjgl.glfw.*;
@@ -21,11 +27,21 @@ public class ConfigXrayScreen extends Screen {
 	BlockList blockList;
 	EditBox addField;
 	Button addButton;
+	public SugList sugList;
 
+	private static final List<String> allBlocks = new ArrayList<>();
+
+	static{
+		for(Block block : Registry.BLOCK){
+			allBlocks.add(block.getRegistryName().toString());
+		}
+		allBlocks.sort(String::compareTo);
+	}
 
 	public ConfigXrayScreen(){
 		super(new TranslatableComponent("title." + Forgetest.ID + ".configXray"));
 	}
+
 
 	@Override
 	protected void init(){
@@ -37,14 +53,43 @@ public class ConfigXrayScreen extends Screen {
 				if(keyCode == GLFW.GLFW_KEY_ENTER){
 					addButton.onPress();
 					return true;
+				}else if(keyCode == GLFW.GLFW_KEY_UP){
+					sugList.keyPressed(GLFW.GLFW_KEY_UP, 0, 0);
+					return true;
+				}else if(keyCode == GLFW.GLFW_KEY_DOWN){
+					sugList.keyPressed(GLFW.GLFW_KEY_DOWN, 0, 0);
+					return true;
+				}else if(keyCode == GLFW.GLFW_KEY_TAB){
+					SugList.SugEntry entry = sugList.getSelected();
+					if(entry != null){
+						setValue(entry.block);
+						sugList.update(getValue());
+					}
+					return false;
 				}
 				return super.keyPressed(keyCode, scanCode, modifier);
 			}
 		};
+		addField.setMaxLength(200);
+		addField.setResponder(content -> sugList.update(content));
 		addButton = new Button(addField.x + addField.getWidth() + 2, addField.y, 30, 20,
 			new TranslatableComponent("title." + Forgetest.ID + ".add"), button ->
 		{
 			String name = addField.getValue();
+			LocalPlayer player=Minecraft.getInstance().player;
+			assert player != null;
+			if(name.equals("hand")){
+				ItemStack item=player.getInventory().getSelected();
+				Block block=Block.byItem(item.getItem());
+				if(block!=Blocks.AIR){
+					addXrayBlock(block);
+				}
+			}else if(name.equals("look")){
+				HitResult result= player.pick(20,0,false);
+				if(result.getType()== HitResult.Type.BLOCK){
+					addXrayBlock(Minecraft.getInstance().level.getBlockState(((BlockHitResult)result).getBlockPos()).getBlock());
+				}
+			}
 			Block block;
 			try{
 				block = ForgeRegistries.BLOCKS.getValue(new ResourceLocation(name));
@@ -52,12 +97,7 @@ public class ConfigXrayScreen extends Screen {
 				return;
 			}
 			if(block != null && block != Blocks.AIR){
-				Keys.enabledBlocks.add(block);
-				addField.setValue("");
-				writeConfig();
-				blockList.updateEntry();
-				if(Keys.xray)
-					Minecraft.getInstance().levelRenderer.allChanged();
+				addXrayBlock(block);
 			}
 
 
@@ -66,6 +106,8 @@ public class ConfigXrayScreen extends Screen {
 			new TranslatableComponent("title." + Forgetest.ID + ".save"), (button) ->
 			Minecraft.getInstance().setScreen(null));
 		blockList = new BlockList();
+		sugList = new SugList();
+		addWidget(sugList);
 		addWidget(blockList);
 		addWidget(addField);
 
@@ -74,6 +116,17 @@ public class ConfigXrayScreen extends Screen {
 		addField.setFocus(true);
 		blockList.updateEntry();
 		super.init();
+	}
+
+	private void addXrayBlock(Block block){
+		Keys.enabledBlocks.add(block);
+		addField.setValue("");
+		writeConfig();
+		blockList.updateEntry();
+		addField.setFocus(true);
+		ConfigXrayScreen.this.setFocused(addField);
+		if(Keys.xray)
+			Minecraft.getInstance().levelRenderer.allChanged();
 	}
 
 	void writeConfig(){
@@ -110,6 +163,25 @@ public class ConfigXrayScreen extends Screen {
 
 	}
 
+	static Set<String> readStringConfig(){
+		Set<String> set = new HashSet<>();
+		try{
+			new File("config/" + Forgetest.ID).mkdirs();
+			new File("config/" + Forgetest.ID + "/xray.txt").createNewFile();
+			BufferedReader br = new BufferedReader(new FileReader("config/" + Forgetest.ID + "/xray.txt"));
+			String line;
+			while((line = br.readLine()) != null){
+				if(!line.isEmpty())
+					set.add(line);
+			}
+			br.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return set;
+
+	}
+
 	@Override
 	public void render(PoseStack stack, int mouseX, int mouseY, float partialTicks){
 		renderBackground(stack);
@@ -120,7 +192,115 @@ public class ConfigXrayScreen extends Screen {
 		blockList.render(stack, mouseX, mouseY, partialTicks);
 		addButton.render(stack, mouseX, mouseY, partialTicks);
 		addField.render(stack, mouseX, mouseY, partialTicks);
+		sugList.render(stack, mouseX, mouseY, partialTicks);
 		super.render(stack, mouseX, mouseY, partialTicks);
+	}
+
+	class SugList extends ObjectSelectionList<SugList.SugEntry> {
+		int w = 0;
+
+		public SugList(){
+			super(Minecraft.getInstance(), ConfigXrayScreen.this.width,
+				ConfigXrayScreen.this.height,
+				addField.y - 90,
+				addField.y - 3,
+				10);
+			setRenderSelection(false);
+			x0 = addField.x;
+			//setRenderBackground(false);
+			setRenderTopAndBottom(false);
+			update("");
+		}
+
+		public void update(String input){
+			clearEntries();
+			if(input.isEmpty()){
+				y0 = addField.y - 30;
+				x1 = 0;
+				setSelected(null);
+				return;
+			}
+			w = 0;
+			Set<String> stored = readStringConfig();
+			for(String t : allBlocks){
+				if(t.contains(input) && !stored.contains(t)){
+					addEntry(new SugEntry(t));
+					w = Math.max(w, font.width(t));
+				}
+			}
+
+			if(children().size() > 0)
+				setSelected(children().get(children().size() - 1));
+			y0 = Mth.clamp(addField.y - children().size() * 10, addField.y - 90, addField.y - 30);
+			setScrollAmount(getMaxScroll());
+			width = w + 20;
+			height = 100;
+			x1 = getScrollbarPosition() + 5;
+		}
+
+
+		@Override
+		public boolean keyPressed(int keyCode, int scanCode, int modifier){
+			if(children().size() < 1)
+				return super.keyPressed(keyCode, scanCode, modifier);
+			if(keyCode == GLFW.GLFW_KEY_UP && getSelected() == children().get(0)){
+				setSelected(children().get(children().size() - 1));
+				ensureVisible(getSelected());
+				return true;
+			}else if(keyCode == GLFW.GLFW_KEY_DOWN && getSelected() == children().get(children().size() - 1)){
+				setSelected(children().get(0));
+				ensureVisible(getSelected());
+				return true;
+			}
+			return super.keyPressed(keyCode, scanCode, modifier);
+		}
+
+		@Override
+		public int getRowLeft(){
+			return addField.x;
+		}
+
+		@Override
+		public int getRowWidth(){
+			return w + 20;
+		}
+
+		@Override
+		protected int getScrollbarPosition(){
+			return getRowRight();
+		}
+
+		class SugEntry extends ObjectSelectionList.Entry<SugEntry> {
+			String block;
+
+			public SugEntry(String block){
+				this.block = block;
+			}
+
+			@Override
+			public void render(PoseStack stack, int index, int top, int left, int width, int height, int mouseX, int mouseY, boolean isMouseOver, float partialTicks){
+				if(top < y0 || top + height > y1){
+					return;
+				}
+				if(isMouseOver){
+					setSelected(this);
+				}
+				font.draw(stack, block, left, top, getSelected() == this ? 0xffff00 : 0xffffff);
+			}
+
+			@Override
+			public boolean mouseClicked(double p_94737_, double p_94738_, int p_94739_){
+				addField.setValue(block);
+				addField.setFocus(true);
+				ConfigXrayScreen.this.setFocused(addField);
+				return super.mouseClicked(p_94737_, p_94738_, p_94739_);
+			}
+
+			@Override
+			public Component getNarration(){
+				return TextComponent.EMPTY;
+			}
+		}
 	}
 
 	class BlockList extends ObjectSelectionList<BlockList.BlockListEntry> {
@@ -141,9 +321,11 @@ public class ConfigXrayScreen extends Screen {
 
 		public void updateEntry(){
 			clearEntries();
-			for(Block t : ConfigXrayScreen.readConfig()){
+			Set<Block> stored = ConfigXrayScreen.readConfig();
+			for(Block t : stored){
 				addEntry(t);
 			}
+			Keys.enabledBlocks = stored;  // fix: lost all configs
 		}
 
 		@Override
@@ -156,6 +338,12 @@ public class ConfigXrayScreen extends Screen {
 				if(Keys.xray)
 					Minecraft.getInstance().levelRenderer.allChanged();
 			}
+		}
+
+		@Override
+		public void render(PoseStack p_93447_, int p_93448_, int p_93449_, float p_93450_){
+			y1 = sugList.getTop() - 15;
+			super.render(p_93447_, p_93448_, p_93449_, p_93450_);
 		}
 
 		class BlockListEntry extends ObjectSelectionList.Entry<BlockListEntry> {
@@ -195,6 +383,10 @@ public class ConfigXrayScreen extends Screen {
 
 	@Override
 	public boolean keyPressed(int keyCode, int scanCode, int modifiers){
+		if(keyCode == GLFW.GLFW_KEY_TAB){
+			addField.keyPressed(keyCode, scanCode, modifiers);
+			return true;
+		}
 		setFocused(addField);
 		return super.keyPressed(keyCode, scanCode, modifiers);
 	}
