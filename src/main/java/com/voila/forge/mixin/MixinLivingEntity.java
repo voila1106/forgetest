7dc70494a30156fd717b59f9213c637a804c7c5a
voila1106 <voila1106@gmail.com>
2022-07-03 13:13:17 +0800

    remove potion particle

--- src/main/java/com/voila/forge/mixin/MixinLivingEntity.java
+++ src/main/java/com/voila/forge/mixin/MixinLivingEntity.java
@@ -3,6 +3,7 @@ package com.voila.forge.mixin;
 import com.voila.forge.*;
 import net.minecraft.client.*;
 import net.minecraft.client.player.*;
+import net.minecraft.network.syncher.*;
 import net.minecraft.world.effect.*;
 import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.decoration.*;
@@ -13,24 +14,33 @@ import org.spongepowered.asm.mixin.injection.callback.*;
 
 @Mixin(LivingEntity.class)
 public abstract class MixinLivingEntity extends Entity {
+	@Shadow @Final private static EntityDataAccessor<Integer> DATA_EFFECT_COLOR_ID;
+	private final LivingEntity _this=(LivingEntity)(Object)this;
 	private MixinLivingEntity(EntityType<?> p_i48580_1_, Level p_i48580_2_){
 		super(p_i48580_1_, p_i48580_2_);
 	}
 
 	/** ignore blindness effect */
 	@Inject(method = "hasEffect", at = @At("HEAD"), cancellable = true)
-	private void i(MobEffect effect, CallbackInfoReturnable<Boolean> info){
+	private void hasEffect(MobEffect effect, CallbackInfoReturnable<Boolean> info){
 		if(effect == MobEffects.BLINDNESS || effect == MobEffects.DARKNESS)
 			info.setReturnValue(false);
 	}
 
 	/** always render name tag to display health value (CustomNameVisible) */
 	@Inject(method = "shouldShowName", at = @At("HEAD"), cancellable = true)
-	private void t(CallbackInfoReturnable<Boolean> info){
+	private void shouldShowName(CallbackInfoReturnable<Boolean> info){
 		LocalPlayer player = Minecraft.getInstance().player;
 		if(player == null)
 			return;
-		if(!((Object)this instanceof ArmorStand) && position().distanceTo(player.position()) < Forgetest.getNameDistance())
+		if(!(_this instanceof ArmorStand) && position().distanceTo(player.position()) < Forgetest.getNameDistance())
 			info.setReturnValue(true);
+	}
+
+	/** remove potion particle */
+	@Inject(method = "tickEffects", at = @At("HEAD"))
+	private void tickEffects(CallbackInfo info){
+		if(_this == Minecraft.getInstance().player)
+			entityData.set(DATA_EFFECT_COLOR_ID,0);
 	}
 }
