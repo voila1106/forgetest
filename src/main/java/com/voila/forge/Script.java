2286373ee041c74cde08f4f10c4dd031501acc0f
voila <voila1106@gmail.com>
2023-04-19 00:35:01 +0800

    Script can operate container

--- src/main/java/com/voila/forge/Script.java
+++ src/main/java/com/voila/forge/Script.java
@@ -2,7 +2,9 @@ package com.voila.forge;
 
 import net.minecraft.*;
 import net.minecraft.client.*;
+import net.minecraft.client.gui.screens.inventory.*;
 import net.minecraft.client.player.*;
+import net.minecraft.world.inventory.*;
 import net.minecraftforge.api.distmarker.*;
 import net.minecraftforge.common.*;
 import net.minecraftforge.event.*;
@@ -25,6 +27,7 @@ public class Script {
 	private long ticks = 1;
 	public long waitTicks = 0;
 	private boolean loop = false;
+	private Minecraft mc = Minecraft.getInstance();
 
 	public static boolean enabled = false;
 	public static String filename = "script.txt";
@@ -68,14 +71,14 @@ public class Script {
 				case "+crouch":
 					put(ticks, () -> {
 						crouch = true;
-						Minecraft.getInstance().options.keyShift.setDown(true);
+						mc.options.keyShift.setDown(true);
 						player.setShiftKeyDown(true);
 					});
 					break;
 				case "-crouch":
 					put(ticks, () -> {
 						crouch = false;
-						Minecraft.getInstance().options.keyShift.setDown(false);
+						mc.options.keyShift.setDown(false);
 						player.setShiftKeyDown(false);
 					});
 					break;
@@ -92,7 +95,7 @@ public class Script {
 					put(ticks, () -> use = false);
 					break;
 				case "use":
-					put(ticks, () -> ((IMinecraft)Minecraft.getInstance()).use());
+					put(ticks, () -> ((IMinecraft) mc).use());
 					break;
 				case "+attack":
 					put(ticks, () -> attack = true);
@@ -101,10 +104,10 @@ public class Script {
 					put(ticks, () -> attack = false);
 					break;
 				case "attack":
-					put(ticks, () -> ((IMinecraft)Minecraft.getInstance()).attack());
+					put(ticks, () -> ((IMinecraft) mc).attack());
 					break;
 				case "pick":
-					put(ticks, () -> ((IMinecraft)Minecraft.getInstance()).pick());
+					put(ticks, () -> ((IMinecraft) mc).pick());
 					break;
 				default:
 					try{
@@ -115,11 +118,24 @@ public class Script {
 							put(ticks, () -> player.setXRot(Float.parseFloat(cmd.substring(6))));
 						}else if(cmd.startsWith("yaw")){
 							put(ticks, () -> player.setYRot(Float.parseFloat(cmd.substring(4))));
+						}else if(cmd.startsWith("click")){
+							String[] token = cmd.split(" "); // 1: slot 2: key 3: type(pick,quick,swap,throw)
+							int slot = Integer.parseInt(token[1]);
+							int key = Integer.parseInt(token[2]);
+							ClickType type;
+							switch(token[3]){
+								case "pick" -> type = ClickType.PICKUP;
+								case "quick" -> type = ClickType.QUICK_MOVE;
+								case "swap" -> type = ClickType.SWAP;
+								case "throw" -> type = ClickType.THROW;
+								default -> throw new IllegalArgumentException("Illegal click type");
+							}
+							put(ticks, () -> clickSlot(slot, key, type));
 						}else if(!cmd.trim().isEmpty() && !cmd.startsWith("#") && !cmd.startsWith("\t") && !cmd.equals("loop")){
 							Forgetest.sendMessage(ChatFormatting.GOLD + "Warning: Cannot resolve line " + (i + 1) + ": " + cmd);
 						}
-					}catch(Exception e){
-						throw new ScriptSyntaxException("Syntax error in line " + (i + 1));
+					}catch(Throwable e){
+						throw new ScriptSyntaxException("Syntax error in line " + (i + 1) + ": " + e.getMessage());
 					}
 			}
 		}
@@ -170,7 +186,7 @@ public class Script {
 			return;
 		if(!enabled)
 			return;
-		if(Minecraft.getInstance().player == null){
+		if(mc.player == null){
 			cancel();
 			return;
 		}
@@ -207,4 +223,10 @@ public class Script {
 		return (tasks.size() - running.size()) + "/" + tasks.size();
 	}
 
+	private void clickSlot(int slot, int key, ClickType clickType){
+		if(mc.screen instanceof AbstractContainerScreen<?> screen && mc.gameMode != null && mc.player != null){
+			int containerId = screen.getMenu().containerId;
+			mc.gameMode.handleInventoryMouseClick(containerId, slot, key, clickType, mc.player);
+		}
+	}
 }
